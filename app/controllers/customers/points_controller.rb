6b11dc6dd02e22003625501a0659bff801f99c7c
kyousuke678 <kyousuke412678@gmail.com>
2023-02-09 04:57:27 +0000

    add: show/edit

--- app/controllers/customers/points_controller.rb
+++ app/controllers/customers/points_controller.rb
@@ -15,6 +15,26 @@ class Customers::PointsController < ApplicationController
     @points = Point.all
   end
   
+  def show
+    @point = Point.find(params[:id])
+  end
+  
+  def edit
+    @point = Point.find(params[:id])
+    if @point.customer_id != current_customer.id
+      redirect_to points_path
+    end
+  end
+  
+  def update
+    @point = Point.find(params[:id])
+    if @point.update(point_params)
+      redirect_to point_path(@point), notice: "You have updated point successfully."
+    else
+      render "edit"
+    end
+  end
+  
   private
 
   def point_params
