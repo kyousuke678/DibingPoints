a610c6342563fd42e00a620939c218e0bb3924fa
kyousuke678 <kyousuke412678@gmail.com>
2023-02-12 03:14:29 +0000

    add: ユーザー管理機能

--- app/controllers/customers/sessions_controller.rb
+++ app/controllers/customers/sessions_controller.rb
@@ -24,4 +24,16 @@ class Customers::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+protected
+
+  def reject_customer
+    @customer = Customer.find_by(email: params[:customer][:email])
+    return if !@customer
+    if @customer.valid_password?(params[:customer][:password])  && @customer.is_deleted
+      flash[:notice] = "退会済みです。再度ご登録をしてご利用ください。"
+      redirect_to new_customer_session_path
+    else
+      customer_session_path
+    end
+  end
 end
