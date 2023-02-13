c8ae9000a9a3318599b135dc8740d65b4b3362c1
kyousuke678 <kyousuke412678@gmail.com>
2023-02-13 08:57:33 +0000

    update: follow機能

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,9 @@ Rails.application.routes.draw do
       resource :favorites, only: [:create, :destroy]
     end
     resources :users, only: [:index,:show,:edit,:update] do
+      resource :relationships, only: [:create, :destroy]
+    	get 'followings' => 'relationships#followings', as: 'followings'
+    	get 'followers' => 'relationships#followers', as: 'followers'
     end
   end
   get 'search' => 'customers/points#search'
