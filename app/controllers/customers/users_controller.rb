70062510bd6e092233da4214380f43f0a61c41e2
kyousuke678 <kyousuke412678@gmail.com>
2023-02-13 07:05:38 +0000

    update: show

--- app/controllers/customers/users_controller.rb
+++ app/controllers/customers/users_controller.rb
@@ -1,14 +1,19 @@
 class Customers::UsersController < ApplicationController
   before_action :authenticate_customer!
-  
+
   def show
     @user = Customer.find(params[:id])
+    @points = @user.points
+    @today_point =  @points.created_today
+    @yesterday_point = @points.created_yesterday
+    @this_week_point = @points.created_this_week
+    @last_week_point = @points.created_last_week
   end
-  
+
   def index
     @users = Customer.all
   end
-  
+
   def edit
     @user = Customer.find(params[:id])
     if @user != current_customer
@@ -24,11 +29,11 @@ class Customers::UsersController < ApplicationController
       render :edit
     end
   end
-  
+
   private
 
   def customer_params
     params.require(:customer).permit(:name, :profile_image, :introduction)
   end
-  
+
 end
